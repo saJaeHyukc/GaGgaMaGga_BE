259c7e9ad6193057155a6a08be28a0b51d01c5e5
ssa <wogur981208@gmail.com>
2023-03-25 15:25:04 +0900

    :sparkles:Feat: user task 예외처리
    -1. 메일링 기능을 실행할 때 예외처리를 추가했습니다
    -2. worker에서 에러가 발생하면 최대 3번까지 실행하고 retry 기본값 시간은
    60초로 설정했습니다
    -3. task를 실행 시 추적이 가능하도록 logger을 적용했습니다.
    info level은 console에만 뜨도록 설정, warning 이상의 level은 console.
    file에 뜨도록 설정
    -4. worker 수를 3개가 작업하도록 docker에 수정했습니다

--- users/tasks.py
+++ users/tasks.py
@@ -1,10 +1,49 @@
-from __future__ import absolute_import, unicode_literals
-
 from celery import shared_task
+from celery.exceptions import MaxRetriesExceededError
 
 from django.core.mail.message import EmailMessage
 
-@shared_task
-def send_email(message):
-    email = EmailMessage(subject=message["email_subject"], body=message["email_body"], to=[message["to_email"]])
-    email.send()
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def send_email(self, message):
+    logger.info(
+        f"Sending email to {message['to_email']}, \
+        subject: {message['email_subject']}, \
+        Task ID: {self.request.id}"
+    )
+
+    try:
+        email = EmailMessage(
+            subject=message["email_subject"],
+            body=message["email_body"],
+            to=[message["to_email"]],
+        )
+        email.send(fail_silently=False)
+
+    except Exception as e:
+        try:
+            self.retry(exc=e)
+            logger.warning(
+                f"Retrying to send email to {message['to_email']}, \
+                    subject: {message['email_subject']}, \
+                    Exception: {e}, \
+                    Retries: {self.request.retries}"
+            )
+
+        except MaxRetriesExceededError:
+            logger.error(
+                f"Failed to send email to {message['to_email']}, \
+                subject: {message['email_subject']}, \
+                Exception: {e}"
+            )
+
+    else:
+        logger.info(
+            f"Email sent successfully to {message['to_email']}, \
+            subject: {message['email_subject']}, \
+            Task ID: {self.request.id}"
+        )
